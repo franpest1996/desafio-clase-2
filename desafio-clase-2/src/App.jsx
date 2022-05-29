221a118ad9194ab3bc3f745512fd64447a351ad6
franpest1996 <franpest1996@gmail.com>
2022-05-29 16:25:28 -0300

    first commit

---
+++ desafio-clase-2/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import NavBar from './Components/NavBar/NavBar'
+
+function App() {
+  return (
+    <>
+      <NavBar/>    
+    </>
+  )
+}
+
+export default App
---
+++ desafio-clase-2/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import './NavBar.css'
+
+function NavBar() {
+    return (
+      <nav className='NavBar'>  
+        <ul>
+            <a href=""><li>Inicio</li></a>
+            <a href=""><li>Productos</li></a>
+            <a href=""><li>Contacto</li></a>
+        </ul>
+      </nav>
+    )
+  }
+
+export default NavBar
