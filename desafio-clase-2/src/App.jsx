08506aa659ff0033134910e980c4cbade029801b
franpest1996 <franpest1996@gmail.com>
2022-06-02 18:12:04 -0300

    first commit

--- desafio-clase-2/src/App.jsx
+++ desafio-clase-2/src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import NavBar from './Components/NavBar/NavBar'
+import Card from './Components/Card/Card'
 
 function App() {
   return (
     <>
-      <NavBar/>    
+      <NavBar/>
+      <Card title='Remera' talle='M' precio={300} />    
     </>
   )
 }
---
+++ desafio-clase-2/src/Components/Card/Card.jsx
@@ -0,0 +1,27 @@
+export default Card
+
+function Card({title, talle, precio}){
+    return(
+        <div className="card">
+            <h2>{title}</h2>
+            <p>{talle}</p>
+            <p>{precio}</p>
+        </div>
+    )
+}
+
+
+// Otra forma
+/*export default function Title(props){
+    const {title, talle, precio} = props
+    
+    return(
+        <div>
+            <h2>{props.title}</h2>
+            <p>{props.talle}</p>
+            <p>{props.precio}</p>
+            
+        </div>
+    )
+} 
+*/
